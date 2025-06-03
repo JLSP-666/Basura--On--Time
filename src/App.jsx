a3eb382d0bb2c50f34dcc4aec984c2406ad4cdc8
SENA <SENA@ARMSCCCCTGOD211.FormacionSena.RED>
2025-06-03 13:42:55 -0500

    fixed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import './App.css'
-import Header from './Layouts/Header/Header'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Usuario from './pages/Usuario/Usuario'
-import ContraR from './pages/ContraR/ContraR'
-import Register from './pages/Register/Register'
-import Admin from './pages/Admin/Admin'
-import PanelAdmin from './pages//PanelAdmin/PanelAdmin'
-import Solicitudes from './pages/Solicitudes/Solicitudes'
-import Rutas from './pages/Rutas/Rutas'
-import Camiones from './pages/Camiones/Camiones'
-import InicioS from './pages/InicioS/InicioS'
-import SolicitudesE from './pages/SolicitudesE/SolicitudesE'
+import { Home } from './pages/Home/Home'
+import { Usuario } from './pages/Usuario/Usuario'
+import { ContraR } from './pages/ContraR/ContraR'
+import { Register } from './pages/Register/Register'
+import { Admin } from './pages/Admin/Admin'
+import { PanelAdmin } from './pages//PanelAdmin/PanelAdmin'
+import { Solicitudes } from './pages/Solicitudes/Solicitudes'
+import { Rutas } from './pages/Rutas/Rutas'
+import { Camiones } from './pages/Camiones/Camiones'
+import { InicioS } from './pages/InicioS/InicioS'
+import { SolicitudesE } from './pages/SolicitudesE/SolicitudesE'
 
 export function App() {
   return (
     <>
       {/* Puedes dejar Header aquí si quieres que esté siempre visible */}
-      
+
 
       <Routes>
-        <Route path="/InicioS" element={<InicioS />} />
-  
         <Route path="/" element={<Home />} />
+        <Route path="/InicioS" element={<InicioS />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Usuario" element={<Usuario />} />
         <Route path="/ContraR" element={<ContraR />} />
