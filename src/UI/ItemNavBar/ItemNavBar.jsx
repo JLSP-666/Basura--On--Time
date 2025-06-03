a8da14766fd602427ffb6ad69bcbdfbe429cb75d
JLPS-666 <jahirsece@gmail.com>
2025-06-03 05:24:44 -0500

    repair 01

--- src/UI/ItemNavBar/ItemNavBar.jsx
+++ src/UI/ItemNavBar/ItemNavBar.jsx
@@ -3,18 +3,11 @@ import './ItemNavBar.css';
 
 export const ItemNavBar = ({ route, icon: Icon, label }) => {
   return (
-<<<<<<< HEAD
     <NavLink to={route} className="itemNavLink">
       <div className="buttonContent">
         {Icon && <Icon className="icon" />}
         <span className="label">{label}</span>
       </div>
-=======
-    <NavLink to={route} className="w-full h-full">
-     <button class="button">
-      
-    </button>
->>>>>>> origin/David_Muñoz
     </NavLink>
   );
 };
